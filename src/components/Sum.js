28794c177079dfd4c6eeb7f5e3acc380723cd935
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-09-23 07:09:04 +0000

    version changed

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -5,12 +5,9 @@ let [sum, setSum] = useState(0);
     return (
         <div>
             <h1>Sum Calculator</h1>
-            <input type="number" onChange={
-                (e) => (
-                    setSum(parseInt(sum) + parseInt(e.target.value))
-                )}/>
+            <input type="number" onChange={(e) => (setSum(parseInt(sum) + parseInt(e.target.value)))}/>
              <p>Sum: {sum}</p>
         </div>
-    )
-}
+    )}
 export default Sum
+
